c04a86143ac14258596ab196e5f649968941a24f
Abhishek27283 <abhishekgupta3257@gmail.com>
2025-09-21 12:03:57 +0530

    print number from 1 to 10.

---
+++ oneToTen.java
@@ -0,0 +1,8 @@
+//Write a program to print numbers from 1 - 10 with each number printed in a separate line.
+public class oneToTen {
+    public static void main(String[] args){
+        for(int i=1;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
