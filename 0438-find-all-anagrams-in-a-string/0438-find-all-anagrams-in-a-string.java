3eaa3760faafca3ba338e8996571a28679c24471
Abhishek27283 <73183253+Abhishek27283@users.noreply.github.com>
2026-06-26 15:00:54 +0530

    Time: 71 ms (18.5%), Space: 48.6 MB (5.24%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,18 +1,23 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
+        HashMap<Character,Integer> HMP = new HashMap<>();
+        HashMap<Character,Integer> HMS = new HashMap<>();
         ArrayList<Integer> res = new ArrayList<>();
         if(p.length()>s.length()) return res;
-        char [] chp = new char[26];
-        char [] chs = new char[26];
+       
         for(int i=0;i<p.length();i++){
-            chp[p.charAt(i)-'a']++;
-            chs[s.charAt(i)-'a']++;
-        }if(Arrays.equals(chp,chs)) res.add(0);
+           char chs = s.charAt(i);
+           char chp =p.charAt(i);
+           HMP.put(chp,HMP.getOrDefault(chp,0)+1);
+
+           HMS.put(chs,HMS.getOrDefault(chs,0)+1);
+        }if(HMS.equals(HMP)) res.add(0);
 
         for(int j=p.length();j<s.length();j++){
-            chs[s.charAt(j)-'a']++;
-            chs[s.charAt(j-p.length())-'a']--;
-            if(Arrays.equals(chs,chp)) res.add(j-p.length()+1);
+            HMS.put(s.charAt(j),HMS.getOrDefault(s.charAt(j),0)+1);
+            HMS.put(s.charAt(j-p.length()),HMS.getOrDefault(s.charAt(j-p.length()),0)-1);
+            if(HMS.get(s.charAt(j-p.length()))==0) HMS.remove(s.charAt(j-p.length()));
+            if(HMS.equals(HMP)) res.add(j-p.length()+1);
         }
         return res;
      
