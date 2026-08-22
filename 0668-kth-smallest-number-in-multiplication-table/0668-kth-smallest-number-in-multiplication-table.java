7444670393579118be2b54635fe65fc71031a6a9
Abhishek27283 <73183253+Abhishek27283@users.noreply.github.com>
2026-08-23 02:15:47 +0530

    Time: 16 ms (7.35%), Space: 42.2 MB (23.88%) - LeetHub

--- 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.java
+++ 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.java
@@ -6,8 +6,8 @@ class Solution {
             int mid = low + (high-low)/2;
             int count =0;
 
-            for(int i =1;i<=Math.min(m,n);i++){
-                count+=Math.min(Math.max(n,m),mid/i);
+            for(int i =1;i<=Math.max(m,n);i++){
+                count+=Math.min(Math.min(n,m),mid/i);
             }
             if(count>=k) high = mid;
             else low =mid+1;
