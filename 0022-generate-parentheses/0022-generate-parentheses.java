ad4ba727b181b05c3ab17423f6b5ed76bccadae7
Abhishek27283 <73183253+Abhishek27283@users.noreply.github.com>
2026-09-26 23:28:15 +0530

    Time: 2 ms (69.24%), Space: 44.2 MB (92.67%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,19 +1,21 @@
 class Solution {
-    private List<String> getValidParanthesis(String s,int open,int close,int n){
-        List<String> result = new ArrayList<>();
+    private void getValidParanthesis(List<String>result ,String s,int open,int close,int n){
         if(open==0 && close==0){
             result.add(s);
-            return result;
+            return ;
         }
         if(open!=0 && open>0){
-          result.addAll(getValidParanthesis(s+"(",open-1,close,n) ) ;
+          getValidParanthesis(result,s+"(",open-1,close,n)  ;
         }
         if(close!=0 && close>open){
-           result.addAll( getValidParanthesis(s+")",open,close-1,n) ) ;
+           getValidParanthesis(result,s+")",open,close-1,n)  ;
         }
-        return result;
+        
     }
     public List<String> generateParenthesis(int n) {
-        return getValidParanthesis("",n,n,n);
+        List<String> result =new ArrayList<>();
+        getValidParanthesis(result,"",n,n,n);
+        return result;
+
     }
 }
